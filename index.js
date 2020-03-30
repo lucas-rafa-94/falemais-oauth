c43708fc6673cbc7b83cff3468fac62cb051ca25
Lucas dos Santos <lucas.rfl.santos.94@gmail.com>
2020-03-30 04:55:48 -0300

    ajustes

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ if (process.env.SCOPE) {
 }
 
 // On successful install, users will be redirected to /oauth-callback
-const REDIRECT_URI = `http://localhost:${PORT}/oauth-callback`;
+const REDIRECT_URI = `https://170.254.79.160:${PORT}/oauth-callback`;
 
 //===========================================================================//
 
