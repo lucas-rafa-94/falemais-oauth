701453fc504626233d48ffeca2520245fe393b5f
Lucas dos Santos <lucas.rfl.santos.94@gmail.com>
2020-04-01 23:32:59 -0300

    ajuste headers

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ app.get('/', async(req, res) => {
 
 
 app.get('/teste', async(req, res) => {
-  res.setHeader('Content-Type', 'text/html');
+  // res.setHeader('Content-Type', 'text/html');
   res.sendFile( __dirname + "/index.html" );
   res.end();
 });
